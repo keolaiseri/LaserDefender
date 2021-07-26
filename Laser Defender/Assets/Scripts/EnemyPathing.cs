85d45e2e6a303f775cb37cab6925caa20566d8fd
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-07-25 15:54:04 -1000

    edit

--- Laser Defender/Assets/Scripts/EnemyPathing.cs
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -26,13 +26,12 @@ public class EnemyPathing : MonoBehaviour
 
     private void Move()
     {
-        if (waypointIndex <= waypoints.Count - 1)
+        if (waypointIndex <= waypoints.Count -1)
         {
             var targetPosition = waypoints[waypointIndex].transform.position;
 
             var movementThisFrame = moveSpeed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards
-                (transform.position, targetPosition, movementThisFrame);
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
 
             if (transform.position == targetPosition)
             {
