fcaf7717267b4e2ddc2b7cb445a57224716aa383
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-07-28 10:24:21 -1000

    Working on Audio

--- Laser Defender/Assets/Scripts/Enemy.cs
+++ Laser Defender/Assets/Scripts/Enemy.cs
@@ -12,6 +12,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] GameObject deathVFX;
     [SerializeField] float durationOfExplosion = 1f;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] [Range(0,1)] float deathSoundVolume = 0.75f;
  
 
 
@@ -80,6 +82,7 @@ public class Enemy : MonoBehaviour
                 transform.rotation
                 );
         Destroy(explosion, durationOfExplosion);
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
 
     }
 }
