6faa585e8dd7fb90deb36bf8adf8913b596d8ee3
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-07-27 18:32:53 -1000

    Working on Parallax

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -52,6 +52,7 @@ public class Player : MonoBehaviour
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
+        damageDealer.Hit();
 
         if (health <= 0)
         {
