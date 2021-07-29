a88a7934f99132863e6de63269d014242e04d4e6
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-07-29 07:37:03 -1000

    start menu complete

--- Laser Defender/Assets/Scripts/Level.cs
+++ Laser Defender/Assets/Scripts/Level.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void LoadStartMenu()
     {
-        
+        SceneManager.LoadScene(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGame()
     {
-        
+
+        SceneManager.LoadScene("Game");
+
+
     }
+
+
+    public void LoadGameOver()
+    {
+
+        SceneManager.LoadScene("Game Over");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
 }
+
+
