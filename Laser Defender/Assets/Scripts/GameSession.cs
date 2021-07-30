530586986e36e1927c209d6b3f0032fa92fb34ce
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-07-30 09:08:17 -1000

    Edits

--- Laser Defender/Assets/Scripts/GameSession.cs
+++ Laser Defender/Assets/Scripts/GameSession.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameSession : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    int score = 0;
+
+
+    private void Awake()
     {
-        
+        SetUpSingleton();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetUpSingleton()
     {
-        
+        int numberGameSessions = FindObjectsOfType<GameSession>().Length;
+        if(numberGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+
+    public void AddToScore(int scoreValue)
+    {
+        score += scoreValue;
+
+    }
+
 }
